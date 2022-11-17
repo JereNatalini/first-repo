2fcaa9a7720d4ab18b02efe419e2f47a0b721a45
JereNatalini <jerenatalini36@gmail.com>
2022-11-17 19:18:15 -0300

    Ejercicios

---
+++ MODULO-1/Ejercicio3/main.js
@@ -0,0 +1,39 @@
+const numero1 = 14;
+const numero2 = 12;
+
+const numero3 = 10;
+const numero4 = 5;
+
+
+function sumar(numero,numero)
+{
+    var suma = numero1 + numero2;
+    document.write("Suma: " + numero1 + "+" + numero2 + "= " + suma + "<br>")
+}
+
+sumar(numero1,numero2);
+
+
+function restar(numero3,numero4)
+{
+    var resta = numero3-numero4;
+    document.write("Resta: " + numero3 + "-" + numero4 + "= " + resta + "<br>")
+}
+
+restar(numero3,numero4);
+
+function multiplicar(numero1,numero4)
+{
+    var multiplicacion = numero1*numero4;
+    document.write("Multiplicacion: " + numero1 + "x" + numero4 + "= " + multiplicacion + "<br>")
+}
+
+multiplicar(numero1,numero4);
+
+function dividir(numero3,numero4)
+{
+    const division = numero3/numero4;
+    document.write("Division: " + numero3 + "/" + numero4 + "= " + division + "<br>")
+}
+
+dividir(numero3,numero4);
