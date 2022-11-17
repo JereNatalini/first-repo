206a2d4382c0b0c4a181eccb88dbf9bdb0f340cc
JereNatalini <jerenatalini36@gmail.com>
2022-11-17 18:53:04 -0300

    Ejercicio 1 y 2, prueba

---
+++ MODULO-1/Ejercicio2/main.js
@@ -0,0 +1,13 @@
+const nombre = 'Jeremias Natalini';
+const edad = 21;
+const mayorDeEdad = true;
+const direccion = {
+    calle : 'Aconcagua',
+    numero : '1234'
+};
+const lenguajesFavoritos = ['JavaScript','C#','Python'];
+const mejorLenguaje = lenguajesFavoritos[0];
+const peorLenguaje = lenguajesFavoritos[2];
+
+
+
