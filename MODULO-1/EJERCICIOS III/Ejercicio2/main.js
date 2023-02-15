c2d46cb9c1d79bffcfa65cd23b7d4f6bdcc315e7
JereNatalini <jerenatalini36@gmail.com>
2023-02-15 18:50:01 -0300

    Ejercicios

--- MODULO-1/EJERCICIOS III/Ejercicio2/main.js
+++ MODULO-1/EJERCICIOS III/Ejercicio2/main.js
@@ -1,1 +1,35 @@
 
+const pinCorrecto = '1234';
+let pin = prompt("Ingrese el PIN:")
+let intentos = 0;
+
+while(intentos < 3)
+{
+    if(pin == pinCorrecto)
+    {
+        document.write("Eso fue correcto")
+        intentos=4;
+    }
+    else
+        pin = prompt("Intente nuevamente:")
+        intentos++;
+}
+
+
+//Intentos infinitos
+
+let pin2 = prompt("Ingrese un PIN:")
+while (pin2 !== 'salir')
+{
+    if (pin2 == pinCorrecto){
+        document.write("Eso fue correcto")
+        break;
+    }
+    else{
+        pin = prompt("Ingrese nuevamente su PIN o escriba salir:")
+    }
+}
+
+
+
+
